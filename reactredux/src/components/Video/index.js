33f6f86f94f274126a55a8bd5dbf93fa7df0587b
Douglas Mendes <capudougmor@gmail.com>
2020-06-17 21:40:28 -0300

    Criada a sidbar

---
+++ reactredux/src/components/Video/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+export default class Video extends Component {
+  render() {
+    return (
+      <div className="App">
+        <strong>Modulo x</strong>
+        <span>Aula</span>
+      </div>
+    )
+  }    
+}
+
