f57b7d5b7df1dcb62489b9fa46e525a0f894d305
Douglas Mendes <capudougmor@gmail.com>
2020-07-15 22:30:57 -0300

    Principio de async await em fundamentos-react

---
+++ fundamentos-react/src/components/AsyncAwait.jsx
@@ -0,0 +1,10 @@
+// const axios = require('axios')
+
+// async function obterPersonagem(id) {
+//     const res = await axios.get('http://swapi.co/api/people${id}')
+//     return res.data
+// }
+
+// obterPersonagem(2).then(dados => {
+//     console.log(dados)
+// })
