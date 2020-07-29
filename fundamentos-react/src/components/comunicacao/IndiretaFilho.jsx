f0125a2751f52405739e5f84e9971e7270c6b4ba
Douglas Mendes <capudougmor@gmail.com>
2020-07-28 21:01:28 -0300

    Estado no react em fundamentos-react

--- fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
+++ fundamentos-react/src/components/comunicacao/IndiretaFilho.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 
 export default (props) => {
   const cb = props.quandoClicar
+  const max = 70
+  const min = 20
+
+  const gerarIdade = () => parseInt(Math.random() * (max - min)) + min
+  const gerarNerd = () => Math.random() > 0.5
   
   return (
     <div>
       <div>
         Filho
       </div>
-      <button onClick={ e => { cb('Joao', 53, true)}
+      <button onClick={ e => { cb('Joao', gerarIdade(), gerarNerd())}
       }>Fornecer Informações</button>
     </div>
   )
--- fundamentos-react/src/components/comunicacao/IndiretaPai.jsx
+++ fundamentos-react/src/components/comunicacao/IndiretaPai.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 import IndiretaFilho from './IndiretaFilho'
 
 export default (props) => {
+  const [nome, setNome] = useState('?')
+  const [idade, setIdade] = useState(0)
+  const [nerd, setNerd] = useState(false)
 
   function fornecerInformaçoes(nome, idade, nerd) {
-    console.log(nome, idade, nerd)
+    setNome(nome)
+    setNerd(nerd)
+    setIdade(idade)
   }
   return (
     <div>
-      <strong>Pai</strong><br/>
+      <strong>Pai</strong><br />
+      <span>{nome} </span>
+      <span><strong>{idade} </strong></span>
+      <span>{nerd ? 'verdadeiro' : 'falso'} </span>
       <IndiretaFilho quandoClicar={fornecerInformaçoes}></IndiretaFilho>
     </div>
   )
