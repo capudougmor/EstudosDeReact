a93303c76d616524e5f0d599377f976f093e37d1
Douglas Mendes Assunção <capudougmor@gmail.com>
2020-07-30 19:46:45 +0000

    Finalizado estudo em useContext

--- NativeUseContex/src/contexts/auth.tsx
+++ NativeUseContex/src/contexts/auth.tsx
@@ -1,10 +1,12 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState, useEffect } from 'react'
+import AsyncStorage from '@react-native-community/async-storage'
 import * as auth from '../services/auth'
 
 interface AuthContextData {
     signed: boolean;
     user: object | null;
-    signIn(): Promise<void>
+    signIn(): Promise<void>;
+    signOut(): void;
 }
 
 const AuthContext = createContext<AuthContextData>({} as AuthContextData)
@@ -12,14 +14,40 @@ const AuthContext = createContext<AuthContextData>({} as AuthContextData)
 export const AuthProvider: React.FC = ({ children }) => {
     const [user, setUser] = useState<object | null>(null)
 
+    useEffect(() => {
+        async function loadStorageData() {
+            const storagedUser = await AsyncStorage.getItem('@RNAuth:user')
+            const storagedToken = await AsyncStorage.getItem('@RNAuth:token')
+
+            
+            if(storagedUser && storagedToken) {
+
+                setUser(JSON.parse(storagedUser))
+            }
+        }
+        
+        loadStorageData()
+    }, [])
+    
     async function signIn() {
         const res = await auth.signin()
-
+        
         setUser(res.user)
+
+        api.defaults.headers['Authorization'] = `Bearer ${res.token}`
+        
+        await AsyncStorage.setItem('@RNAuth:user', JSON.stringify(res.user))
+        await AsyncStorage.setItem('@RNAuth:token', res.token)
+    }
+    
+    function signOut() {
+        AsyncStorage.clear().then(() => {
+            setUser(null)
+        })
     }
 
     return (
-        <AuthContext.Provider value={{ signed: !!user, user, signIn }}>
+        <AuthContext.Provider value={{ signed: !!user, user, signIn, signOut }}>
             {children}
         </AuthContext.Provider>
     )
--- NativeUseContex/src/pages/Dashboard/index.tsx
+++ NativeUseContex/src/pages/Dashboard/index.tsx
@@ -1,8 +1,26 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useContext } from "react";
+import { View, Button, StyleSheet } from "react-native";
+import AuthContext from "../../contexts/auth";
+
+const styles = StyleSheet.create({
+    container: {
+        justifyContent: "center",
+        alignContent: "center",
+        flexGrow: 1,
+    }
+});
 
 const Dashboard: React.FC = () => {
-  return <View />;
+    const { signOut } = useContext(AuthContext)
+
+    function handleSignOut() {
+        signOut()
+    }
+    return (
+        <View style={styles.container}>
+            <Button title="Logout" onPress={ handleSignOut } />
+        </View>
+    );
 };
 
 export default Dashboard;
