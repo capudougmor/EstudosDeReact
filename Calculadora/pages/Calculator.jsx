bd3c9ebf5edfc9310a849e3605332a1f2a3b71ed
Douglas Mendes <capudougmor@gmail.com>
2020-08-28 17:36:58 -0300

    Finalizada calculadora

--- Calculadora/pages/Calculator.jsx
+++ Calculadora/pages/Calculator.jsx
@@ -15,7 +15,7 @@ const initialState = {
 export default class Calculator extends Component {
 
   state = { ...initialState }
-  
+
   constructor(props) {
     super(props)
 
@@ -28,28 +28,50 @@ export default class Calculator extends Component {
     this.setState({ ...initialState })
   }
 
-  setOperation(n) {
-    console.log(n);
+  setOperation(operation) {
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true })
+    } else {
+      const equals = operation === '='
+      const currentOperation = this.state.operation
+
+      const values = [...this.state.values]
+      try {
+        values[0] = eval(`${values[0]} ${currentOperation} ${values[1]} `)
+      } catch(e) {
+        values[0] = this.state.values[0]
+      }
+      values[1] = 0
+
+      this.setState({
+        displayValue: values[0],
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        clearDisplay: !equals,
+      })
+      return values
+    }
   }
 
   addDigit(n) {
-    if(n === '.' && this.state.displayValue.includes('.')) {
+    if (n === '.' && this.state.displayValue.includes('.')) {
       return
     }
 
-    const clearDisplay = this.state.displayValue ==='0'
+    const clearDisplay = this.state.displayValue === '0'
       || this.state.clearDisplay
     const currentValue = clearDisplay ? '' : this.state.displayValue
     const displayValue = currentValue + n
     this.setState({ displayValue, clearDisplay: false })
 
-    if(n !== '.' ) {
+    if (n !== '.') {
       const i = this.state.current
       const newValue = parseFloat(displayValue)
       const values = [...this.state.values]
       values[i] = newValue
       this.setState({ values })
     }
+    console.log(this.state.values);
   }
 
 
