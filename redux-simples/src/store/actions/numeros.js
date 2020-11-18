126a2553d922f4e3c1b693fe00d22d4d7a1acaa7
Douglas Mendes <capudougmor@gmail.com>
2020-11-18 15:45:37 -0300

    criado backend de my-money

--- redux-simples/src/store/actions/numeros.js
+++ redux-simples/src/store/actions/numeros.js
@@ -1,13 +1,15 @@
+import { NUM_MAX_ALTERADO, NUM_MIN_ALTERADO } from './actionsTypes'
+
 export function alterarNumeroMin(novoNum) {
   return {
-    type: 'NUM_MIN_ALTERADO',
+    type: NUM_MIN_ALTERADO,
     payload: novoNum
   }
 }
 
 export function alterarNumeroMax(novoNum) {
   return {
-    type: 'NUM_MAX_ALTERADO',
+    type: NUM_MAX_ALTERADO,
     payload: novoNum
   }
 }
