486fab780007bd2117711ce3ad4e02c327c12894
Douglas Mendes <capudougmor@gmail.com>
2020-11-25 20:18:02 -0300

    Iniciado frontend em my-money

---
+++ my-money-app/frontend/src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+import App from './main/app.js'
+
+ReactDom.render(<App></App>, document.getElementById('app'))
---
+++ my-money-app/frontend/src/main/app.js
@@ -0,0 +1,11 @@
+import '../common/template/dependencies'
+import React from 'react'
+
+import Header from '../common/template/header.jsx'
+
+export default props => (
+  <div className="wrapper">
+    <h1>App</h1>
+    <Header />
+  </div>
+)
